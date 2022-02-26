8d62cb4625b3a2cea57542cf082d9e25e7e6bbc8
Hugo Perronnet <hugo.perronnet@polymtl.ca>
2022-02-26 15:00:06 -0500

    pdated project

--- app/src/main/java/de/danoeh/antennapod/fragment/preferences/StatisticsFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/preferences/StatisticsFragment.java
@@ -16,28 +16,25 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.activity.PreferenceActivity;
+import de.danoeh.antennapod.fragment.PagedToolbarFragment;
 
 /**
  * Displays the 'statistics' screen
  */
-public class StatisticsFragment extends Fragment {
+public class StatisticsFragment extends PagedToolbarFragment {
 
     public static final String TAG = "StatisticsFragment";
 
-    private static final int POS_LISTENED_HOURS = 0;
-    private static final int POS_SPACE_TAKEN = 1;
-    private static final int TOTAL_COUNT = 2;
-
+    private static final int POS_SUBSCRIPTIONS = 0;
+    private static final int POS_YEARS = 1;
+    private static final int POS_SPACE_TAKEN = 2;
+    private static final int TOTAL_COUNT = 3;
 
     private TabLayout tabLayout;
     private ViewPager2 viewPager;
     private Toolbar toolbar;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
@@ -46,29 +43,28 @@ public class StatisticsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.pager_fragment, container, false);
         viewPager = rootView.findViewById(R.id.viewpager);
         toolbar = rootView.findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.statistics_label));
+        toolbar.inflateMenu(R.menu.statistics);
+        toolbar.setNavigationOnClickListener(v -> getParentFragmentManager().popBackStack());
         viewPager.setAdapter(new StatisticsPagerAdapter(this));
         // Give the TabLayout the ViewPager
         tabLayout = rootView.findViewById(R.id.sliding_tabs);
+        super.setupPagedToolbar(toolbar, viewPager);
         new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
             switch (position) {
-                case POS_LISTENED_HOURS:
-                    tab.setText(R.string.playback_statistics_label);
+                case POS_SUBSCRIPTIONS:
+                    tab.setText(R.string.subscriptions_label);
+                    break;
+                case POS_YEARS:
+                    tab.setText(R.string.years_statistics_label);
                     break;
                 case POS_SPACE_TAKEN:
-                    tab.setText(R.string.download_statistics_label);
+                    tab.setText(R.string.downloads_label);
                     break;
                 default:
                     break;
             }
         }).attach();
-
-        if (getActivity().getClass() == PreferenceActivity.class) {
-            rootView.findViewById(R.id.toolbar).setVisibility(View.GONE);
-        } else {
-            toolbar.setTitle(getString(R.string.statistics_label));
-            toolbar.setNavigationOnClickListener(v -> getParentFragmentManager().popBackStack());
-        }
-
         return rootView;
     }
 
@@ -90,8 +86,10 @@ public class StatisticsFragment extends Fragment {
         @Override
         public Fragment createFragment(int position) {
             switch (position) {
-                case POS_LISTENED_HOURS:
-                    return new PlaybackStatisticsFragment();
+                case POS_SUBSCRIPTIONS:
+                    return new SubscriptionStatisticsFragment();
+                case POS_YEARS:
+                    return new YearsStatisticsFragment();
                 default:
                 case POS_SPACE_TAKEN:
                     return new DownloadStatisticsFragment();
